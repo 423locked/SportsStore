3cec83392c34445b66e704327410915cd73689d8
Yaroslav Prishedko <yaroslav05042005@gmail.com>
2021-02-17 16:57:32 +0300

    Fixed pagination. Backend works fine

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
@@ -12,10 +13,25 @@ namespace SportsStore.Controllers
         // инициализация интерфейса IProductRepository как repository
         // объекты Product --> IProductRepository --> FakeProductRepository (DB)
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository repo) { repository = repo; }
 
         // Передаем в представление repository.Products (по сути таблицу продуктов из БД)
-        public ViewResult List() => View(repository.Products);
+        public ViewResult List(int productPage = 1) => View(new ProductsListViewModel
+        {
+            Products = repository.Products
+            .OrderBy(p => p.ProductID)
+            .Skip((productPage - 1) * PageSize)
+            .Take(PageSize),
+
+            PagingInfo = new PagingInfo
+            {
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalItems = repository.Products.Count()
+            }
+        });
+            
     }
 }
